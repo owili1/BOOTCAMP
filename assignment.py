ef1966635ef489d8b8b96bb817b9c99ce6fa1c9f
owili1 <86737057+owili1@users.noreply.github.com>
2021-07-07 14:57:36 +0300

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,26 @@
+
+a=1
+print(a)
+a=a+a
+print(a)
+a=a+a
+print(a)
+a=a+a
+print(a)
+a=True
+print(a)
+a=not a
+print(a)
+a=not a
+print(a)
+a=not a
+print(a)
+a=2
+print(a)
+a=a*a
+print(a)
+a=a*a
+print(a)
+a=a*a
+print(a)
+
---
+++ variable.py
@@ -0,0 +1,4 @@
+a=b=c=17
+print(a)
+print(b)
+print(c)
