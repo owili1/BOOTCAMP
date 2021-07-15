de430b650f73aa4f58326acf28547121cb29af2a
owili1 <86737057+owili1@users.noreply.github.com>
2021-07-15 13:49:44 +0300

    Add files via upload

---
+++ datatypes q5 6.py
@@ -0,0 +1,31 @@
+a=b=c=17
+print(a)
+print(b)
+print(c)
+
+#Question6
+a=1
+print(a)
+a=a+a
+print(a)
+a=a+a
+print(a)
+a=a+a
+print(a)
+a=True
+print(a)
+a=not a
+print(a)
+a=not a
+print(a)
+a=not a
+print(a)
+a=2
+print(a)
+a=a*a
+print(a)
+a=a*a
+print(a)
+a=a*a
+print(a)
+
