cddc8b15f52478f9bacebbb405e4355f9bb34f8c
owili1 <86737057+owili1@users.noreply.github.com>
2021-07-15 13:48:49 +0300

    Add files via upload

---
+++ Hello world.py
@@ -0,0 +1,7 @@
+print("Hello World\n"*10)
+
+#modifying hello world to prinT nmaes in reverse
+name1=(input("Enter name"))
+name2=(input("Enter name"))
+name3=(input("Enter name"))
+print("Hi "+name3+","+name2+","+name1+".")
